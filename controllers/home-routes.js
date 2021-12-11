189f647eaecde3621649d1c7894161e7760bbfad
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-11 14:31:45 -0700

    added dashboard routes and minor tweaks

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -99,12 +99,12 @@ router.get("/posts-comments", (req, res) => {
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ["username"],
+          attributes: ["email"],
         },
       },
       {
         model: User,
-        attributes: ["username"],
+        attributes: ["email"],
       },
     ],
   })
--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,24 @@
 // this file collects the packaged API routes and prefixes them with the path /api
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./api/user-routes');
-const homeRoutes = require('./home-routes');
-const postRoutes = require('./api/post-routes');
-const commentRoutes = require('./api/comment-routes');
-
+// const userRoutes = require('./api/user-routes');
+// const postRoutes = require('./api/post-routes');
+// const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require("./home-routes");
+const apiRoutes = require("./api");
+const dashboardRoutes = require("./dashboard-routes");
 
 // taking routes and implementing them to another router instance and prefixing the path with /users
-router.use('/api/users', userRoutes);
-router.use('/', homeRoutes);
-router.use('/api/posts', postRoutes);
-router.use('/api/comments', commentRoutes);
-
-
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
+// router.use('/api/users', userRoutes);
+// router.use('/api/posts', postRoutes);
+// router.use('/api/comments', commentRoutes);
 
 // this second router.use is a RESTful API practice  indicating a request that doesn't exist
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
-
-
-
